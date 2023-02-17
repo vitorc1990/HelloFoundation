767db236188b4bd34fb27ae753085105517cd5ac
Vitor Oliveira <vitorc1990@gmail.com>
2023-02-17 17:25:26 -0300

    Initial Commit

---
+++ HelloFoundation/HelloFoundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloFoundationApp.swift
+//  HelloFoundation
+//
+//  Created by user on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloFoundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
